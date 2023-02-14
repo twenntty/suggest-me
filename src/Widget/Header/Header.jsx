4e3ff7db4bc3eddb53920596f620c51ce9434601
mr-Maksim0001k <maksimadamenko54@gmail.com>
2023-02-14 20:57:51 +0200

    Test

--- src/Widget/Header/Header.jsx
+++ src/Widget/Header/Header.jsx
@@ -2,6 +2,7 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
   return <header className={styles.header}>Header</header>;
+  return <header className={styles.header}>Headerr</header>;
 };
 
 export default Header;
