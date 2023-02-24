c0bafdcdbe40cec2abe14b47381181f67150c641
MARKUS <89963327+twenntty@users.noreply.github.com>
2023-02-24 18:22:43 +0200

    change poster

--- src/Components/UI/BigPoster/BigPoster.jsx
+++ src/Components/UI/BigPoster/BigPoster.jsx
@@ -1,8 +1,10 @@
 import s from "./BigPoster.module.scss";
 
 const BigPoster = ({backdrop}) => {
+
     return ( 
-        <div className={s.big_poster} style={{ background: `linear-gradient(180deg, rgba(54, 44, 146, 0.4) 0%, rgba(18, 98, 151, 0.4) 100%), url(${backdrop})` }}>
+        <div className={s.big_poster} style={{ background: `linear-gradient(180deg, rgba(54, 44, 146, 0.4) 0%, rgba(18, 98, 151, 0.4) 100%)` }}>
+            <img src={backdrop} alt="backdrop" className={s.backdrop} />
         </div>
      );
 }
