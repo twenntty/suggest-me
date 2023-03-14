e4d96a7ad3ac9fa59ad9d843541e3a0c427e035c
MARKUS <89963327+twenntty@users.noreply.github.com>
2023-03-14 13:52:08 +0200

    navbar

--- src/Widget/Header/Header.jsx
+++ src/Widget/Header/Header.jsx
@@ -1,6 +1,7 @@
 import styles from "./Header.module.scss";
 import icons from "../../assets/icons/logo.svg";
 import arrow from "../../assets/icons/arrow-right.svg";
+import logout from "../../assets/icons/logout.svg";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -9,6 +10,24 @@ const Header = () => {
             <img src={icons} alt="logo"/>
             </Link>
 
+            if(localStorage.accessToken){
+              <div className={styles.menu}>
+                  <div className={styles.menu}>
+                    <Link to="Dashboard">
+                      
+                    </Link>
+                    <a href="#">Suggestions</a>
+                    <a href="#">Add</a>
+                  </div>
+                  <div className={styles.logout}>
+                      <Link>
+                        <img src={logout} alt="logout" />
+                        <span>Logout</span>
+                      </Link>
+                  </div>
+              </div>
+            }
+            else {
             <div className={styles.menu}>
               <div className={styles.tablets}>
                   <a href="#">Movies</a>
@@ -20,7 +39,8 @@ const Header = () => {
                   <img src={arrow} alt="arrow" />
                   </Link>
                 </div>
-            </div>
+            </div> 
+            }
         </header>;
 };
 
